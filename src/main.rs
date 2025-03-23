4f01bd18d4b5aaf5cbb07fd510ca8edad1aa60b9
aritlhq <aritlhq@gmail.com>
2025-03-23 08:25:43 +0700

    test: add tests for variable handling in Rust

--- src/main.rs
+++ src/main.rs
@@ -8,3 +8,49 @@ fn main() {
 fn hello_test() {
     println!("Hello, world everyone!");
 }
+
+/**
+ * Variables
+ *
+ * Variables are used to store data.
+ * Variables are immutable by default.
+ */
+#[test]
+fn test_variable() {
+    let name = "Ari";
+    println!("Hello, {}!", name);
+}
+
+
+/**
+ * Mutable Variables
+ * 
+ * Variables can be mutable, meaning they can be changed.
+ * To make a variable mutable, you need to use the `mut` keyword.
+ */
+#[test]
+fn test_mutable() {
+    let mut name = "Ari Cool";
+    println!("Hello, {}!", name);
+
+    name = "Ari COooOOoooOoOoOolllll";
+    println!("Hello, {}!", name);
+
+    // name = 10; // Error
+    // println!("Hello, {}!", name);
+}
+
+/**
+ * Shadowing
+ *
+ * Variables can be shadowed, meaning they can be redefined.
+ * To shadow a variable, you need to use the `let` keyword.
+ */
+#[test]
+fn shadowing() {
+    let name = "Ari Cool!";
+    println!("Hello, {}!", name);
+
+    let name = "Ari Cool Shadowing!";
+    println!("Hello, {}!", name);
+}
